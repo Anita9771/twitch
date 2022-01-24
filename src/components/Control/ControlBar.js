33358d47540c41e80bbbca48fda19f3e4945376d
Anita9771 <ieghonghon@gmail.com>
2022-01-24 18:12:55 +0100

    new changes

--- src/components/Control/ControlBar.js
+++ src/components/Control/ControlBar.js
@@ -30,7 +30,7 @@ const ControlBar = () => {
   };
   
   const toggleScreen = async () => {
-    await hmsActions.setScreenShareEnabled(true);
+    await hmsActions.setScreenShareEnabled(!isLocalScreenShared);
   }
 
   return (
@@ -80,13 +80,10 @@ const ControlBar = () => {
 
       <button
       className='text-xs uppercase tracking-wider bg-white py-1 px-2 rounded-lg shadow-lg text-iwhite ml-2 bg-red-600'
-      onClick={() => {
-          hmsActions.leave();
+      onClick={() => { hmsActions.endRoom(false, "session ended") && hmsActions.leave();
         }}
     >
-     {
-       isModerator ? "End" : "Leave"
-     }
+     End
     </button>
        </> 
      </div>
--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -8,7 +8,7 @@ export default async function getToken( role) {
       body: JSON.stringify({
         user_id: '5fc62c5872909272bf9995e1', // User ID assigned by you (different from 100ms' assigned id)
         role: role, // listener , speaker , moderator
-        room_id: "61c79831a0fa766d7f542dda"
+        room_id: "61eedaa5d85bf05ee4e1767f"
       }),
     });
   
